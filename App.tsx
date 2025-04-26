f97d8a86a730744ca7d23f4052050cd661d3d860
brian-h2 <brianheredia200309@gmail.com>
2025-04-26 00:16:59 -0300

    Subida de archivos tanto backend como front

--- App.tsx
+++ App.tsx
@@ -1,20 +1,80 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { useState, useEffect } from 'react';
+import { View, StyleSheet, Dimensions, ActivityIndicator, Alert } from 'react-native';
+import MapView, { Marker, Region } from 'react-native-maps';
+import * as Location from 'expo-location';
 
 export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-      <StatusBar style="auto" />
+    const [location, setLocation] = useState<Location.LocationObject | null>(null);
+    const [region, setRegion] = useState<Region | null>(null);
+    const [loading, setLoading] = useState(true);
+
+    useEffect(() => {
+      (async () => {
+        let { status } = await Location.requestForegroundPermissionsAsync();
+        if (status !== 'granted') {
+          alert('Permiso de ubicacion degenegado');
+          return;
+        }
+
+        let loc = await Location.getCurrentPositionAsync({});
+        console.log('Ubicación obtenida:', loc);
+
+        setLocation(loc);
+        setRegion({
+          latitude: loc.coords.latitude,
+          longitude: loc.coords.longitude,
+          latitudeDelta: 0.01,
+          longitudeDelta: 0.01,
+        });
+        setLoading(false);
+      })(); //Esto permite la ejecucion del async
+    }, [])
+
+    const colectivosSimulados = [
+      { id: 1, lat: -34.6037, lng: -58.3816 }, // Obelisco
+      { id: 2, lat: -34.6091, lng: -58.3923 }, // Congreso
+      { id: 3, lat: -34.6012, lng: -58.3841 }, // Teatro Colón
+    ];
+
+    if (loading || !region) {
+      return (
+        <View  style={styles.centered}>
+            <ActivityIndicator size="large" color="#42w87f5"/>
+        </View>
+      )
+    }
+
+    return (
+      <View style={styles.container}>
+      <MapView style={styles.map} region={region} showsUserLocation>
+        {colectivosSimulados.map(colectivo => (
+          <Marker
+            key={colectivo.id}
+            coordinate={{ latitude: colectivo.lat, longitude: colectivo.lng }}
+            title={`Colectivo ${colectivo.id}`}
+            pinColor="blue"
+          />
+        ))}
+      </MapView>
     </View>
-  );
+    )
+
+
+
+
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
+  },
+  map: {
+    width: Dimensions.get('window').width,
+    height: Dimensions.get('window').height,
+  },
+  centered: {
+    flex: 1,
     justifyContent: 'center',
+    alignItems: 'center',
   },
 });
---
+++ backend-colectivos/index.js
@@ -0,0 +1,29 @@
+const express = require('express');
+const cors = require('cors');
+const app = express();
+
+const PORT = process.env.PORT || 5000;
+
+app.use(cors());
+
+const colectivosSimulados = [
+    { id: 1, lat: -34.6037, lng: -58.3816 }, // Obelisco
+    { id: 2, lat: -34.6091, lng: -58.3923 }, // Congreso
+    { id: 3, lat: -34.6012, lng: -58.3841 }, // Teatro Colón
+  ];
+
+function MovimientoColectivos() {
+    colectivosSimulados.forEach(colectivo => {
+        colectivo.lat += (Math.random() - 0.5) * 0.001;
+        colectivo.lng += (Math.random() - 0.5) * 0.001;
+    })
+}
+
+app.get('/colectivos', (req, res) => {
+    MovimientoColectivos();
+    res.json(colectivosSimulados);
+});
+
+app.listen(PORT, () => {
+    console.log(`Backend escuchando en puerto ${PORT}`);
+});
